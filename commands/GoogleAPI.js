d1422c7b8e63a278de7569fd2f370911cde2eeb1
DamocStefan <91914348+DamocStefan@users.noreply.github.com>
2021-11-23 19:24:51 +0200

    make it better

--- commands/GoogleAPI.js
+++ commands/GoogleAPI.js
@@ -25,7 +25,6 @@ async function getValuesFromSpreadSheet() {
   });
   return getRows.data.values;
 }
-<<<<<<< HEAD
 var sortAlphabets = function (text) {
   return text.split('').sort().join('');
 };
@@ -71,36 +70,11 @@ async function getHoursForGivenName(nume, project) {
             }
           }
           return [-2, "", ""];
-=======
-async function getHoursForGivenName(nume, project) {
-  let values = await getValuesFromSpreadSheet();
-  var projectNumber=1;
-  var k=0;
-  for (let i = 2; i < values.length; i++) {
-    if (nume === values[i][0]) {
-      if(values != 1){
-          for( let j = 0 ; j < 50; j++){
-            if(project == values[0][j])
-              {
-                projectNumber = j;
-                console.log(values[i][j]);
-                break;
-              }
-          }
->>>>>>> parent of c0571a4 (<Versiunea 3 aproximari de numa>)
         }
-<<<<<<< HEAD
-        return [values[i][1] , values[i][0], ""];
+        return [Nimicto0(values[i][1]) , values[i][0], ""];
       }
-=======
-      return values[i][projectNumber];
->>>>>>> 8d055d29df55ac88cdc7529f610362ad11afd14c
     }
   }
-<<<<<<< HEAD
   return [-1, "", ""];
-=======
-  return "NU EXISTA NUMELE";
->>>>>>> parent of c0571a4 (<Versiunea 3 aproximari de numa>)
 }
 module.exports = { getHoursForGivenName };
